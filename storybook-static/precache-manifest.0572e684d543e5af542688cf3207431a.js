7cd9c000332c37684e58a62b09b3463981a76c47
dennis <52568215+denniswebdel@users.noreply.github.com>
2020-11-09 22:08:57 +0000

    TaskBox UI

---
+++ storybook-static/precache-manifest.0572e684d543e5af542688cf3207431a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b471ef8491fb20edc55113db257d3910",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.4d263dbf761f7461e02c.bundle.js"
+  },
+  {
+    "url": "runtime~main.4d263dbf761f7461e02c.bundle.js"
+  },
+  {
+    "revision": "e4c17c0e832c1010c7d2",
+    "url": "static/css/main.f2b987d9.chunk.css"
+  },
+  {
+    "url": "vendors~main.4d263dbf761f7461e02c.bundle.js"
+  },
+  {
+    "url": "vendors~main.4d263dbf761f7461e02c.bundle.js.LICENSE.txt"
+  }
+]);
